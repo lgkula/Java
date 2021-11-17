aabadaf53b45b21b643d3f3c426cd23e458c76f8
Leszek <leslawkula@gmail.com>
2021-11-17 16:52:47 +0100

    Java exercises

---
+++ 20211117-10/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+          
+    public static void main(String[] args) {
+//	System.out.println("Witaj po reinstalacji.");
+
+//    Scanner redaktor = new Scanner(System.in);
+//    System.out.print("Podaj imię redaktora: ");
+//    String redaktorImie = redaktor.nextLine();
+//    System.out.println("Witam cię redaktorze " + redaktorImie);
+//    for(int i=0; i <= 100; i++){
+//        System.out.println(i+"%");
+//    }
+//
+//    Scanner redaktor = new Scanner(System.in);
+//    System.out.print("Podaj imię redaktora: ");
+//    String redaktorImie = redaktor.nextLine();
+//    System.out.println("Podane imię to: " + redaktorImie);
+//    Scanner redaktor = new Scanner(System.in);
+//    System.out.print("Podaj imie: ");
+//    String redaktorImie = redaktor.nextLine();
+//    System.out.println("Podane imie to " + redaktorImie);
+
+    }
+
+}
