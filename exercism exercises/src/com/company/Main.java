0da195bc2d501ed763f7daac241f908c22bf1f46
Leszek <leslawkula@gmail.com>
2021-11-26 13:50:40 +0100

    Java exercises

---
+++ exercism exercises/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import com.tools.AnnalynsInfiltration;
+
+public class Main {
+
+    public static void main(String[] args) {
+	
+        AnnalynsInfiltration wynik = new AnnalynsInfiltration();
+        System.out.println("wynik = " + wynik);
+    }
+}
