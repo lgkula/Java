96cb74bd3672133c06ad13022fffa17c97d017af
Leszek <leslawkula@gmail.com>
2021-11-29 13:53:00 +0100

    Java exercises

--- exercism exercises/src/com/company/Main.java
+++ exercism exercises/src/com/company/Main.java
@@ -5,7 +5,7 @@ import com.tools.AnnalynsInfiltration;
 public class Main {
 
     public static void main(String[] args) {
-	
+
         AnnalynsInfiltration wynik = new AnnalynsInfiltration();
         System.out.println("wynik = " + wynik);
     }
---
+++ exercism exercises/src/com/tools/Lasagne.java
@@ -0,0 +1,27 @@
+package com.tools;
+
+public class Lasagne {
+
+        public Lasagne() {
+            System.out.println("Konstruktor!");
+        }
+
+
+            // TODO: define the 'expectedMinutesInOven()' method
+        public int expectedMinutesInOven() {
+            return 40;
+        }
+
+        // TODO: define the 'remainingMinutesInOven()' method
+        public int remainingMinutesInOven(int minutesInOven) {
+            return expectedMinutesInOven() - minutesInOven;
+        }
+        // TODO: define the 'preparationTimeInMinutes()' method
+        public static int preparationTimeInMinutes(int layers) {
+            return layers * 2;
+        }
+        // TODO: define the 'totalTimeInMinutes()' method
+        public int totalTimeInMinutes(int layers, int minutesInOven) {
+            return preparationTimeInMinutes(layers) + minutesInOven;
+        }
+    }
