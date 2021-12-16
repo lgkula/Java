583ecb9375d301b9b769dcee65d680659183b9af
Leszek <leslawkula@gmail.com>
2021-12-16 13:49:36 +0100

    Java exercises

---
+++ exercism exercises/src/com/tools/AnnalynsInfiltration.java
@@ -0,0 +1,36 @@
+package com.tools;
+
+public class AnnalynsInfiltration {
+    public static boolean canFastAttack(boolean knightIsAwake) {
+        // throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canFastAttack() method");
+        if(knightIsAwake == false) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+        // throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canSpy() method");
+        if(knightIsAwake == true || archerIsAwake == true || prisonerIsAwake == true) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+        // throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canSignalPrisoner() method");
+        if(prisonerIsAwake == true && archerIsAwake == false) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
+        // throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canFreePrisoner() method");
+        if((petDogIsPresent == true && archerIsAwake == false) || (petDogIsPresent == false && prisonerIsAwake == true && archerIsAwake == false && knightIsAwake == false)){
+            return true;
+        }
+        return false;
+    }
+}
+
